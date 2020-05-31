153c372337d495d8931c03e0c3990a4e9254a0d6
Duncan Macleod <duncan.macleod@ligo.org>
2020-05-31 10:11:16 +0100

    enable windows builds

---
+++ recipe/bld.bat
@@ -0,0 +1,24 @@
+:: use local build folder
+mkdir _build
+cd _build
+
+:: configure
+cmake .. ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes ^
+    -DENABLE_SWIG_JAVA:BOOL=no ^
+    -DENABLE_SWIG_MATLAB:BOOL=no ^
+    -DENABLE_SWIG_OCTAVE:BOOL=no ^
+    -DENABLE_SWIG_PYTHON2:BOOL=no ^
+    -DENABLE_SWIG_PYTHON3:BOOL=no
+if errorlevel 1 exit 1
+
+cmake --build . --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ctest --extra-verbose --output-on-failure
+if errorlevel 1 exit 1
+
+cmake --build . --parallel %CPU_COUNT% --target install
+if errorlevel 1 exit 1
---
+++ recipe/install-python.bat
@@ -0,0 +1,24 @@
+setlocal EnableDelayedExpansion
+
+:: use local build folder
+mkdir build
+cd build
+
+:: configure
+cmake "%SRC_DIR%" ^
+  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
+  -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes ^
+  -DENABLE_SWIG_JAVA:BOOL=no ^
+  -DENABLE_SWIG_MATLAB:BOOL=no ^
+  -DENABLE_SWIG_OCTAVE:BOOL=no ^
+  -DENABLE_SWIG_PYTHON3:BOOL=yes
+
+# build
+cmake --build python --parallel "%CPU_COUNT%"
+
+# install
+cmake --build python --parallel "%CPU_COUNT%" --target install
+
+# test
+ctest --extra-verbose --output-on-failure
