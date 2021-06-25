31f2ff65139c50d6bb6aca7c7c9295b60882d3d3
Duncan Macleod <duncan.macleod@ligo.org>
2021-06-25 09:59:31 +0100

    update cmake scripts

--- recipe/install-python.bat
+++ recipe/install-python.bat
@@ -5,18 +5,18 @@ cd "%_builddir%"
 
 :: configure
 cmake "%SRC_DIR%" ^
-    -G "NMake Makefiles" ^
-    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
-    -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes ^
-    -DCMAKE_DISABLE_FIND_PACKAGE_Java:BOOL=yes ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
-    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -Dnds2-client_DIR:PATH="%LIBRARY_BIN%" ^
-    -DENABLE_SWIG_JAVA:BOOL=no ^
-    -DENABLE_SWIG_MATLAB:BOOL=no ^
-    -DENABLE_SWIG_OCTAVE:BOOL=no ^
-    -DENABLE_SWIG_PYTHON2:BOOL=no ^
-    -DENABLE_SWIG_PYTHON3:BOOL=yes
+	-G "NMake Makefiles" ^
+	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
+	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=yes ^
+	-DCMAKE_DISABLE_FIND_PACKAGE_Java:BOOL=yes ^
+	-DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
+	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+	-Dnds2-client_DIR:PATH="%LIBRARY_BIN%" ^
+	-DENABLE_SWIG_JAVA:BOOL=no ^
+	-DENABLE_SWIG_MATLAB:BOOL=no ^
+	-DENABLE_SWIG_OCTAVE:BOOL=no ^
+	-DENABLE_SWIG_PYTHON2:BOOL=no ^
+	-DENABLE_SWIG_PYTHON3:BOOL=yes
 if errorlevel 1 exit 1
 
 :: build
@@ -28,5 +28,5 @@ cmake --build python --parallel "%CPU_COUNT%" --verbose --target install
 if errorlevel 1 exit 1
 
 :: test
-ctest --extra-verbose --output-on-failure
+ctest --parallel "%CPU_COUNT%" --extra-verbose --output-on-failure
 if errorlevel 1 exit 1
